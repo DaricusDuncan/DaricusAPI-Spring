04d4a86bc9cfa449336d32b36ff17da9c96d3251
Daricus Duncan <daricusduncan@gmail.com>
2023-07-01 20:54:06 -0400

    initial setup

---
+++ src/main/java/com/daricusapi/daricusapi/DaricusapiApplication.java
@@ -0,0 +1,13 @@
+package com.daricusapi.daricusapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaricusapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaricusapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/daricusapi/daricusapi/DaricusapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daricusapi.daricusapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaricusapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
